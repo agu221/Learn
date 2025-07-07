6adcc14918dce5d7940ac4fa045612bb45bf9a78
Agu221 <agufero@gmail.com>
2025-07-07 16:31:35 +0200

    Finished back GO tutorial

--- Sports Notification App/greetings/greetings.go
+++ Sports Notification App/greetings/greetings.go
@@ -1,40 +1,39 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
- 	"errors"
 	"math/rand"
 )
 
-
 // Hello returns a greeting for the named person.
-func Hello(name string) (string,error) {
+func Hello(name string) (string, error) {
 	// If no name was given, return an error with a message.
-	if name == ""{
+	if name == "" {
 		return "", errors.New("empty name")
 	}
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
-func Hellos(names []string) (map[string]string,error) {
+func Hellos(names []string) (map[string]string, error) {
 	// If no name was given, return an error with a message.
 	messages := make(map[string]string)
-	for _, name := range names{
+	for _, name := range names {
 		message, err := Hello(name)
-		if err !=nil{
-			return nil,err
+		if err != nil {
+			return nil, err
 		}
 		messages[name] = message
 	}
 	return messages, nil
 }
 
-func randomFormat() string{
-	formats:= []string{
+func randomFormat() string {
+	formats := []string{
 		"Hi, %v. Welcome!",
 		"Great to see you, %v!",
 		"Hail, %v! Well met!",
-		}
+	}
 
 	return formats[rand.Intn(len(formats))]
 }
