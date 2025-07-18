cf92c0c1b9896574815c61eccfa5b712ccd42b14
Agu221 <agufero@gmail.com>
2025-07-18 12:00:27 +0200

    Added Loging in function and ability to connect to postgres db

--- sports_team_manager/models/users.go
+++ sports_team_manager/models/users.go
@@ -3,7 +3,7 @@ package models
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 20)
 	return string(hash), err
 }
 
